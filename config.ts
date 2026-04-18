2be2e3890b27ab9593be10fc988809369f974293
Vishnu-5757 <vishnujrn@gmail.com>
2026-04-18 15:55:34 +0530

    fix: add config.ts with API base URL

---
+++ config.ts
@@ -0,0 +1,4 @@
+// export const API_BASE_URL = 'http://192.168.43.205:8000';
+// export const API_BASE_URL = 'http://192.168.1.8:8000';
+// export const API_BASE_URL = 'http://3.25.162.35:8000';
+export const API_BASE_URL = 'http://3.25.162.35';
